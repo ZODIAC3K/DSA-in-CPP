98e7e56e74e4fc39b6bd29c09eae90396e8c2d01
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2024-03-13 04:34:59 +0530

    explained trailing zero in factorial of a number

--- Learning GFG/tralling_Zero.cpp
+++ Learning GFG/tralling_Zero.cpp
@@ -1,34 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long factorial(int n){
-    if(n==0 || n==1){
-        return 1;
-    }
-    return n*factorial(n-1);
-}
+// Novice approach
 
-int main(){
-    long long val;
+// long long factorial(int n){
+//     if(n==0 || n==1){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+
+// int main(){
+//     long long val;
+//     cin >> val;
+    
+//     long long n =  factorial(val);
+
+//     long long r = 0;
+//     long long count = 0;
+
+//     while(r == 0 && n>=0){
+
+//         r = n%10;
+
+//         if (r == 0){
+//             count++;
+//         }
+//         n = n/10;
+//     }
+
+//     cout << count << endl;
+// }
+
+// Optimized approach
+int main (){
+    long long val, count = 0;
     cin >> val;
     
-    long long n =  factorial(val);
+    // we are solving without calculating the factorial. 
+    // Total no of 5's is always less than the no of 2's in the factorial of a numbe so we are counting the no of 5's in the factorial of the given number. we are adding questient of val/5, val/25, val/125, val/625, val/3125, val/15625, val/78125, val/390625, val/1953125 which is the total no of 5's in the factorial of the given number.
 
-    long long r = 0;
-    long long count = 0;
-
-    while(r == 0 && n>=0){
-
-        r = n%10;
-
-        if (r == 0){
-            count++;
-        }
-        n = n/10;
+    for (long long i = 5; i <= val; i = i * 5){ 
+        count = count + val/i;
     }
-
     cout << count << endl;
 }
-
-
 
