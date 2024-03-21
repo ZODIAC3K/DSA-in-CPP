9aca2e0ccfb236acbb78ccbd1c641a783fc6269f
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2024-03-22 00:46:34 +0530

    Sum_of_Array_elements

---
+++ Learning GFG/Sum_of_Array_elements.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution{
+public:
+
+	int sum(int arr[], int n) {
+	    int sum = 0;
+	    for(int i = 0; i < n; i++){
+	        sum = sum + arr[i];
+	    }
+	    return sum;
+	}
+};
+
+
+
+int main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
